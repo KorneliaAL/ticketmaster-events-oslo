c30c75e67a16b1a4e12288f4d7acbc6434a57bd9
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-02-25 16:46:22 +0100

    Added toggle visibility function for navigation

---
+++ _app/js/modules/header.js
@@ -0,0 +1,33 @@
+export default function Header() {
+	let navigationVisible = false;
+
+	const headerNavigation = document.querySelector('.header__navigation');
+	const headerButtonMenu = document.querySelector('.header__navigation-menu-button');
+
+	headerButtonMenu.addEventListener('click', handleHeaderButtonMenuClick);
+
+	function handleHeaderButtonMenuClick(event) {
+		toggleNavigation();
+		renderHTML();
+	}
+
+	function toggleNavigation() {
+		navigationVisible = !navigationVisible;
+	}
+
+/**
+ * @todo add changes to burger icon to exit icon, and vice versa
+ */
+	function renderHTML() {
+		if (navigationVisible === true) {
+			headerNavigation.classList.add('header__navigation--visible');
+			// headerButtonMenu.style.backgroundImage = 'url(../../assets/icons/exit-menu.svg)';
+			// headerButtonMenu.setAttribute('aria-label', 'Lukk');
+
+		} else {
+			headerNavigation.classList.remove('header__navigation--visible');
+			// headerButtonMenu.style.backgroundImage = 'url(../../assets/icons/burger-menu.svg)';
+			// headerButtonMenu.setAttribute('aria-label', 'Meny');
+		}
+	}
+}
