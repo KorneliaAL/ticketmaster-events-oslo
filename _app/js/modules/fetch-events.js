39bb720fbeee69bec513d5581af66090dbd2d6f0
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-03-02 14:30:00 +0100

    Removed comment

--- _app/js/modules/fetch-events.js
+++ _app/js/modules/fetch-events.js
@@ -1,6 +1,6 @@
 export default async function FetchEvents() {
 	let city = 'oslo';
-	let size = 200;
+	let size = 50;
 	const key = 'r0y0Y9JY3l9MjW0ntmnIo0RMSfK7eoZu';
 	const endpoint = `https://app.ticketmaster.com/discovery/v2/events?apikey=${key}&locale=*&size=${size}&city=${city}&countryCode=NO&segmentName=music`;
 
@@ -20,16 +20,4 @@ export default async function FetchEvents() {
 	});
 
 	return filteredData;
-	// const container = document.querySelector('.events');
-
-	// result._embedded.events.forEach(event => {
-	// 		const img = document.createElement('img');
-	// 		img.setAttribute('src', event.images[3].url);
-
-	// 	img.classList.add('event__img');
-	// 		container.appendChild(img);
-		
-	// });
-
-	// console.log(result._embedded.events);
 }
