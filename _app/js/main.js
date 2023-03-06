2a49fac1b7059e295b04594ed5fe9cd630be227a
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-03-06 15:01:33 +0100

    Imported filterEvents

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,7 +1,9 @@
 import Header from './modules/header.js';
 import FetchEvents from './modules/fetch-events.js';
 import RenderEvents from './modules/render-events.js';
+import FilterEvents from './modules/filter-events.js';
 
 Header();
 const eventData = await FetchEvents();
 RenderEvents(eventData);
+FilterEvents(eventData);
