7b0c29679ee029cdfab27569c7b8c40478674fa4
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-03-07 11:14:56 +0100

    Created button element and returned to HTML

--- _app/js/modules/create-genre-buttons.js
+++ _app/js/modules/create-genre-buttons.js
@@ -1,3 +1,71 @@
 export default function CreateGenreButtons() {
-	console.log('button');
+	const buttonContainer = document.querySelector('.events__filter-element-container');
+	const genreButtons = [
+		{
+			name: 'Theatre'
+		},
+		{
+			name: 'Alternative'
+		},
+		{
+			name: 'Classical'
+		},
+		{
+			name: 'Theatre'
+		},
+		{
+			name: 'Rock'
+		},
+		{
+			name: 'Folk'
+		},
+		{
+			name: 'Pop'
+		},
+		{
+			name: 'Cultural'
+		}
+		,
+		{
+			name: 'Jazz'
+		},
+		{
+			name: 'World'
+		},
+		{
+			name: 'Folk'
+		},
+		{
+			name: 'Dance'
+		},
+		{
+			name: 'Hip-Hop/Rap'
+		},
+		{
+			name: 'Blues'
+		},
+		{
+			name: 'Dance/Electronic'
+		}
+	];
+
+
+	if (buttonContainer) {
+		renderHTML();
+	}
+			
+	function returnButtonDOMElement(button) {
+		const buttonElement = document.createElement('button');
+		buttonElement.classList.add('events__filter-element-button');
+		buttonElement.innerText = button.name;
+
+		return buttonElement; 
+	}
+
+	function renderHTML() {
+		genreButtons.forEach(button => {
+			const buttonElement = returnButtonDOMElement(button);
+			buttonContainer.appendChild(buttonElement);	
+		});
+	}
 }
