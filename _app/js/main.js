dee48de705293630e41b70ef8b20fd9456fbc6f2
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-03-08 14:31:36 +0100

    Changed file name

--- _app/js/main.js
+++ _app/js/main.js
@@ -2,10 +2,9 @@ import Header from './modules/header.js';
 import FetchEvents from './modules/fetch-events.js';
 import RenderEvents from './modules/render-events.js';
 import FilterEvents from './modules/filter-events.js';
-import CreateGenreButtons from './modules/create-genre-buttons.js';
 
 Header();
 const eventData = await FetchEvents();
 RenderEvents(eventData);
-CreateGenreButtons();
-// FilterEvents(eventData);
+FilterEvents(eventData);
+
