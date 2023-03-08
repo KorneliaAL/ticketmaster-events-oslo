317e8de064e86849b9a2648c5157a1066a160540
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-03-08 14:32:04 +0100

    Created filter functionality for events

--- _app/js/modules/filter-events.js
+++ _app/js/modules/filter-events.js
@@ -1,3 +1,149 @@
-export default function FilterEvents() {
-	console.log('hello')
+// import FilterEvents from "./filter-events";
+import RenderEvents from "./render-events.js";
+export default function FilterEvents(data) {
+	const sectionContainer = document.querySelector('.events__container');
+	const buttonContainer = document.querySelector('.events__filter-element-container');
+	const genreButtons = [
+		{
+			name: 'All'
+		},
+		{
+			name: 'Theatre'
+		},
+		{
+			name: 'Alternative'
+		},
+		{
+			name: 'Classical'
+		},
+		{
+			name: 'Theatre'
+		},
+		{
+			name: 'Rock'
+		},
+		{
+			name: 'Folk'
+		},
+		{
+			name: 'Pop'
+		},
+		{
+			name: 'Cultural'
+		},
+		{
+			name: 'Jazz'
+		},
+		{
+			name: 'World'
+		},
+		{
+			name: 'Folk'
+		},
+		{
+			name: 'Dance'
+		},
+		{
+			name: 'Hip-Hop/Rap'
+		},
+		{
+			name: 'Blues'
+		},
+		{
+			name: 'Dance/Electronic'
+		}
+	];
+
+	if (buttonContainer) {
+		renderHTML();
+	}
+
+	function handleButtonElementClick(event) {
+		const currentButton = event.target.innerText
+		filterEvents(currentButton);
+	}
+
+	function filterEvents(currentGenre) {
+		let filteredEvents = [];
+		switch (currentGenre) {
+			case 'All': 
+				filteredEvents = data;
+				break;
+
+			case 'Theatre':
+				filteredEvents = data.filter(event => event.genre[0].genre.name=== 'Theatre');
+				break;
+
+			case 'Alternative':
+				filteredEvents = data.filter(event => event.genre[0].genre.name=== 'Alternative');
+				break;
+			
+			case 'Classical':
+				filteredEvents = data.filter(event => event.genre[0].genre.name=== 'Classical');
+				break;
+			
+			case 'Theatre':
+				filteredEvents = data.filter(event => event.genre[0].genre.name=== 'Theatre');
+				break;
+
+			case 'Rock':
+				filteredEvents = data.filter(event => event.genre[0].genre.name=== 'Rock');
+				break;
+
+			case 'Folk':
+				filteredEvents = data.filter(event => event.genre[0].genre.name=== 'Folk');
+				break;
+
+			case 'Pop':
+				filteredEvents = data.filter(event => event.genre[0].genre.name=== 'Pop');
+				break;
+
+			case 'Cultural':
+				filteredEvents = data.filter(event => event.genre[0].genre.name=== 'Cultural');
+				break;
+
+			case 'Jazz':
+				filteredEvents = data.filter(event => event.genre[0].genre.name=== 'Jazz');
+				break;
+
+			case 'World':
+				filteredEvents = data.filter(event => event.genre[0].genre.name=== 'World');
+				break;
+
+			case 'Dance':
+				filteredEvents = data.filter(event => event.genre[0].genre.name=== 'Dance');
+				break;
+
+			case 'Hip-Hop/Rap':
+				filteredEvents = data.filter(event => event.genre[0].genre.name=== 'Hip-Hop/Rap');
+				break;
+
+			case 'Blues':
+				filteredEvents = data.filter(event => event.genre[0].genre.name=== 'Blues');
+				break;
+
+			case 'Dance/Electronic':
+				filteredEvents = data.filter(event => event.genre[0].genre.name=== 'Dance/Electronic');
+				break;
+		}
+
+		sectionContainer.innerText = '';
+		RenderEvents(filteredEvents);
+	}
+			
+	function returnButtonDOMElement(button) {
+		const buttonElement = document.createElement('button');
+		buttonElement.classList.add('events__filter-element-button');
+		buttonElement.innerText = button.name;
+
+		buttonElement.addEventListener('click', handleButtonElementClick);
+		return buttonElement; 
+	}
+
+	function renderHTML() {
+		genreButtons.forEach(button => {
+			const buttonElement = returnButtonDOMElement(button);
+			buttonContainer.appendChild(buttonElement);	
+		});
+	}
 }
