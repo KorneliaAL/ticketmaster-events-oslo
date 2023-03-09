899f533321c26fe7125e6f6bf297487e5de0fb37
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-03-09 15:09:11 +0100

    Created try catch, and added error messages

--- _app/js/modules/fetch-events.js
+++ _app/js/modules/fetch-events.js
@@ -2,26 +2,47 @@ import { tm_token } from "../env.js";
 export default async function FetchEvents() {
 	
 	let city = 'oslo';
-	let size = 51;
+	let size = 198;
 	const x = tm_token;
 	const endpoint = `https://app.ticketmaster.com/discovery/v2/events?apikey=${x}&locale=*&size=${size}&city=${city}&countryCode=NO&segmentName=music`;
 
-	const response = await fetch(endpoint);
-	const result = await response.json();
-	const events = result._embedded.events;
-	const filteredData = events.map(event => {
-		return {
-			artist: event.name,
-			venue: event._embedded,
-			genre: event.classifications,
-			minPrice: event.priceRanges,
-			date: event.dates.start.localDate,
-			time: event.dates.start.localTime,
-			image: event.images,
-			timeStamp: event.dates.start.dateTime,
-			url: event.url
-		}
-	});
+	const response = await fetch(endpoint);	
 
-	return filteredData;
+	try {
+		return await handleResponse(response)
+	} catch (error) {
+		const container = document.querySelector('.events');
+		container.textContent = error.message;
+	}
+
+	async function	handleResponse(response) {
+		if (response.ok) {
+			const result = await response.json();
+			const events = result._embedded.events;
+			const filteredData = events.map(event => {
+				return {
+					artist: event.name,
+					venue: event._embedded,
+					genre: event.classifications,
+					minPrice: event.priceRanges,
+					date: event.dates.start.localDate,
+					time: event.dates.start.localTime,
+					image: event.images,
+					timeStamp: event.dates.start.dateTime,
+					url: event.url
+				}
+				
+			});
+			return filteredData;
+		} else if (response.status === 401) {
+			throw new Error('Beklager, du har ikke tilgang til denne siden');
+		} else if (response.status === 404) {
+			throw new Error ('Denne siden finnes ikke');
+		} else if (response.status > 499) {
+			throw new Error('Server er nede, prøv igjen senere');
+		} else {
+			throw new Error('Noe gikk galt, prøv igjen senere');
+		}
+	}
 }
+
